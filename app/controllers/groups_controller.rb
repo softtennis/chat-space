0459cda85dacb659cd2ad746616992f8e3f9ef28
softtennis <softtennis.persona@gmail.com>
2020-08-20 14:34:31 +0900

    send_message

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,7 +24,7 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
@@ -36,3 +36,5 @@ class GroupsController < ApplicationController
   end
 
 end
+
+# @groups = groups.find(session[:groups_id])
