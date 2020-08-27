f8ae286614a0c5d055f4596678c03610657f2e2a
softtennis <softtennis.persona@gmail.com>
2020-08-27 18:28:05 +0900

    implement_Ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,67 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html = 
+        `<div class="main-chat__message1">
+            <span class="user">
+              ${message.user_name}
+            </span>
+            <span class="date">
+              ${message.created_at}
+            </span>
+            <div class="main-chat__message2">
+              <p class="main-chat__message2-content">
+                ${message.content}
+              </p>
+              <img class="main-chat__message2-image" src="${message.image}">
+            </div>
+          </div>`
+      return html;
+    } else {
+      let html =
+          `<div class="main-chat__message1">
+          <span class="user">
+            ${message.user_name}
+          </span>
+          <span class="date">
+            ${message.created_at}
+          </span>
+          <div class="main-chat__message2">
+            <p class="main-chat__message2-content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
+
+  $('.main-chat__message-form').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      let html = buildHTML(message)
+      $('.main-chat__message-list').append(html);
+      $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+      $('.main-chat__message-form')[0].reset();
+      $('.main-chat__submit').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+  });
+});
+
+// json.user_name @message.user.name
+// json.created_at @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
+// json.content @message.content
+// json.image @message.image_url
