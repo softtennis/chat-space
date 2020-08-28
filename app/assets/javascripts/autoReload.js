5507d46f5f945132ca9d93e7e41255f96d8e258e
softtennis <softtennis.persona@gmail.com>
2020-08-28 16:34:38 +0900

    implement_Automatic-update

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html = 
-        `<div class="main-chat__message1">
+        `<div class="main-chat__message1" data-message-id=${message.id}>
             <span class="user">
               ${message.user_name}
             </span>
@@ -19,7 +19,7 @@ $(function(){
       return html;
     } else {
       let html =
-          `<div class="main-chat__message1">
+          `<div class="main-chat__message1" data-message-id=${message.id}>
           <span class="user">
             ${message.user_name}
           </span>
@@ -36,27 +36,29 @@ $(function(){
     };
   }
 
-  $('.main-chat__message-form').on('submit', function(e){
-    e.preventDefault();
-    let formData = new FormData(this);
-    let url = $(this).attr('action');
+  let reloadMessages = function() {
+    let last_message_id = $('.main-chat__message1:last').data("message-id") || 0;
+    console.log(last_message_id)
     $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
+      url: "api/messages",
+      type: 'get',
       dataType: 'json',
-      processData: false,
-      contentType: false
+      data: {id: last_message_id}
     })
-    .done(function(message){
-      let html = buildHTML(message)
-      $('.main-chat__message-list').append(html);
-      $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
-      $('.main-chat__message-form')[0].reset();
-      $('.main-chat__submit').prop('disabled', false);
+    .done(function(messages) {
+      console.log(messages);
+      if (messages.length !== 0) {
+        let insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main-chat__message-list').append(insertHTML);
+        $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+      }
     })
     .fail(function() {
-      alert("メッセージ送信に失敗しました");
+      alert('error');
     });
-  });
+  };
+  setInterval(reloadMessages, 7000);
 });
